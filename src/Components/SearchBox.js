43c918f8a817404eec18b29d80606880b321df60
unknown <anaminformatika21@gmail.com>
2022-10-14 09:31:35 +0700

    add commit

---
+++ src/Components/SearchBox.js
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { fetchSearchCocktails } from "../Redux/features/cocktailSlice";
+
+const SearchBox = () => {
+  const searchTerm = useRef();
+  const dispatch = useDispatch();
+
+  const handleChange = () => {
+    const searchText = searchTerm.current.value;
+    dispatch(fetchSearchCocktails({ searchText }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="d-flex flex-column align-items-center justify-content-center mt-4">
+      <h1>My Cocktail</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type=""
+              ref={searchTerm}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Search Sekarang"
+              style={{ width: "350px" }}
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default SearchBox;
